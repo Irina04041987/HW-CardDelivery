5f576bbb2f9f6f7aee26ce8b28506f0bec3d965b
Irina Platonova <crazyangeli@yandex.ru>
2021-04-05 20:06:56 +0300

    change CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,14 +1,16 @@
 package ru.netology;
 
+import jdk.vm.ci.meta.Local;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -19,20 +21,16 @@ public class CardDeliveryTest {
     void shouldSubmitRequestWithDateAsString() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
-        Calendar cal = new GregorianCalendar();
-        cal.add(Calendar.DAY_OF_YEAR, 5); // увеличиваем на 5 дней от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String dateForTest = format1.format(cal.getTime());
-        for (int i = 0; i < 10; i++) {
-            $("input[placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
-        }
+        LocalDate date = LocalDate.now().plusDays(5);
+        String dateForTest = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("input[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("input[placeholder=\"Дата встречи\"]").val(dateForTest);
         $("[data-test-id=name] input").setValue("Рябова Екатерина");
         $("[data-test-id=phone] input").setValue("+79129487713");
         $("[data-test-id=agreement]").click();
         $$("button[type=\"button\"]").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
-
+        $("div.notification__content").shouldHave(text(dateForTest));
     }
 
 }
